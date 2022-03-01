f44e0107d50a1db3b715f9d44d91c6058b7dac4e
Heze <64218603+Hcmwebs@users.noreply.github.com>
2022-03-01 23:43:46 +1300

    imported all theassets to the App and I passed them as props to the Card Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import Attributions from './Components/Attributions';
 import Card from './Components/Card';
+import equilibrium from './images/image-equilibrium.jpg';
+import ethereum from './images/icon-ethereum.svg';
+import clock from './images/icon-clock.svg';
+import avatar from './images/image-avatar.png';
 
 const App = () => {
 	return (
 		<main className='container'>
-			<Card />
+			<Card
+				equilibrium={equilibrium}
+				ethereum={ethereum}
+				clock={clock}
+				avatar={avatar}
+			/>
 			<Attributions />
 		</main>
 	);
