caac2d188c780663b54be7235b6923dfacae1f71
Heze <64218603+Hcmwebs@users.noreply.github.com>
2022-03-01 23:45:42 +1300

    added elements to the card Header component
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

---
+++ src/Components/CardHeader.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const CardHeader = ({ active,equilibrium}) => {
+	return (
+		<section className='card-header'>
+			<div className={active ? 'imgContainer active' : 'imgContainer'}>
+				<img src={equilibrium} alt='Equilibrium' className='img' />
+			</div>
+		</section>
+	);
+};
+
+export default CardHeader;
