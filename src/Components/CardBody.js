ceb8281bfea4275bd542238753953d43a1ce4fff
Heze <64218603+Hcmwebs@users.noreply.github.com>
2022-03-02 00:07:55 +1300

    updated the CardBody  component
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/Components/CardBody.js
+++ src/Components/CardBody.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
 
-const CardBody = () => {
-  return (
-    <div>CardBody</div>
-  )
-}
+const CardBody = ({active,ethereum,clock}) => {
+	return (
+		<section className='card-body'>
+			<h1 className={active ? 'active ' : ''}>Equilibrium #3429</h1>
+			<p>Our Equilibrium collection promotes balance and calm.</p>
+			<div className='preview'>
+				<button className='btn btn-currency' type='button'>
+					<img src={ethereum} alt='ethereum icon' />
+					0.041 ETH
+				</button>
+				<button className='btn btn-timer' type='button'>
+					<img src={clock} alt='clock icon' />3 days left
+				</button>
+			</div>
+		</section>
+	);
+};
 
-export default CardBody
+export default CardBody;
